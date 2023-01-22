3fb35a86921b761550e0cb765fdf5e07986ba423
chatupon <khochatupon@gmail.com>
2023-01-22 18:25:55 +0700

    add iban service

---
+++ src/Domain/Banking.Domain.Service/AccountLogic/IBAN_Service.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Banking.Domain.Service.AccountLogic
+{
+    public class IBAN_Service : IIBAN_Service
+    {
+        public async Task<string> GenerateIBAN()
+        {
+            string iban;
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            // ref https://sites.google.com/chromium.org/driver/downloads
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"D:\chromedriver_win32_109", "chromedriver.exe");
+            using (var driver = new ChromeDriver(service, options))
+            {
+                driver.Navigate().GoToUrl("http://randomiban.com/?country=Netherlands");
+                iban = driver.FindElement(By.Id("demo")).Text;
+            }
+
+            return iban;
+        }
+    }
+}
