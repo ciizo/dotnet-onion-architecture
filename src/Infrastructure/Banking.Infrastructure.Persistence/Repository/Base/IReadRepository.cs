8a11c5133a0866170847cdd7ef6f1a9d98f387a4
chatupon <khochatupon@gmail.com>
2023-01-22 14:53:22 +0700

    add repository and uow

---
+++ src/Infrastructure/Banking.Infrastructure.Persistence/Repository/Base/IReadRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace Banking.Infrastructure.Persistence.Repository.Base
+{
+    public interface IReadRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get();
+
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> predicate, bool withTracking = true);
+
+        TEntity GetById(object id);
+
+        Task<IEnumerable<TEntity>> GetAsync();
+
+        Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate, bool withTracking = true);
+
+        Task<TEntity> GetByIdAsync(object id);
+
+        IQueryable<TEntity> GetQueryable();
+
+        IQueryable<TEntity> GetQueryable(Expression<Func<TEntity, bool>> predicate, bool withTracking = true);
+    }
+}
---
+++ src/Infrastructure/Banking.Infrastructure.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Banking.Infrastructure.Persistence.UnitOfWork
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext
+    {
+        public TContext Context { get; }
+
+        public UnitOfWork(TContext context)
+        {
+            Context = context;
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            await Context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            await Context.Database.CommitTransactionAsync();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
